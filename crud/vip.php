eb07db6d4d157ad4b959ea37794c5d5b9bfd50a1
saracruzfdez <saracruzfdez@gmail.com>
2023-06-23 10:15:31 +0200

    vip ok

--- crud/vip.php
+++ crud/vip.php
@@ -23,15 +23,15 @@ require_once 'inc/header.inc.php';
 <div class="vip">
     <div class="vip1">
         <p class="scf-textVip1">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis nesciunt excepturi quae non consectetur voluptas officia, facilis illum tempore odio rem obcaecati quo labore veritatis magnam voluptates maxime accusantium qui? Quis nesciunt excepturi quae non consectetur voluptas officia, facilis illum tempore odio rem obcaecati quo labore veritatis magnam voluptates maxime accusantium qui? Quis nesciunt excepturi quae non consectetur voluptas officia, facilis illum tempore odio rem obcaecati quo labore veritatis magnam voluptates maxime accusantium qui? Quis nesciunt excepturi quae non consectetur voluptas officia, facilis illum tempore odio rem obcaecati quo labore veritatis magnam voluptates maxime accusantium qui? Quis nesciunt excepturi quae non consectetur voluptas officia, facilis illum tempore odio rem obcaecati quo labore veritatis magnam voluptates maxime accusantium qui?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis nesciunt excepturi quae non consectetur voluptas officia, facilis illum tempore odio rem obcaecati quo labore veritatis magnam voluptates maxime accusantium qui? Quis nesciunt excepturi quae non consectetur voluptas officia, facilis illum tempore odio rem obcaecati quo labore veritatis magnam voluptates maxime accusantium qui? Quis nesm obcaecati quo labore veritatis magnam voluptates maxime accusantium qui? Quis nesciunt excepturilabore veritatis magnam voluptates maxime accusantium qui?
         </p>
-        <img class="pl-4" src="assets\img\Group 6.png" alt="">
+        <img class="pl-4 scf-imgVip1" src="assets\img\Group 6.png" alt="">
     </div>
 
     <div class="vip2">
-        <img class="pr-4" src="assets\img\Group 5.png" alt="">
+        <img class="pr-4 scf-imgVip2" src="assets\img\Group 5.png" alt="">
         <p class="scf-textVip2">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis nesciunt excepturi quae non consectetur voluptas officia, facilis illum tempore odio rem obcaecati quo labore veritatis magnam voluptates maxime accusantium qui? Quis nesciunt excepturi quae non consectetur voluptas officia, facilis illum tempore odio rem obcaecati quo labore veritatis magnam voluptates maxime accusantium qui? Quis nesciunt excepturi quae non consectetur voluptas officia, facilis illum tempore odio rem obcaecati quo labore veritatis magnam voluptates maxime accusantium qui? Quis nesciunt excepturi quae non consectetur voluptas officia, facilis illum tempore odio rem obcaecati quo labore veritatis magnam voluptates maxime accusantium qui? Quis nesciunt excepturi quae non consectetur voluptas officia, facilis illum tempore odio rem obcaecati quo labore veritatis magnam voluptates maxime accusantium qui?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis nesciunt excepturi quae non consectetur voluptas officia, facilis illum tempore odio rem obcaecati quo labore veritatis magnam voluptates maxime accusantium qui? Quis nesciunt excepturi quae non consectetur voluptas officia, facilis illum tqui? Quis nesciunt excepturi quae non consectetur voluptas officia, facilis illum tempore odio rem obcaecati quo labore veritatis magnam voluptates maxime accusantium qui?
         </p>
     </div>
 </div>
